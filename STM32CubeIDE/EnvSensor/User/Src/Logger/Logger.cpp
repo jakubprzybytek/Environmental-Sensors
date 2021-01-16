659b9722b5a80a5428e80d3ad6c68de8f64dcb44
Jakub Przybytek <jakub.przybytek@gmail.com>
2021-01-16 11:53:11 +0100

    Logging time now

--- STM32CubeIDE/EnvSensor/User/Src/Logger/Logger.cpp
+++ STM32CubeIDE/EnvSensor/User/Src/Logger/Logger.cpp
@@ -9,13 +9,21 @@
 #include <string.h>
 #include <stdio.h>
 
+extern RTC_HandleTypeDef hrtc;
+
 uint8_t Logger::init() {
 	return fileAppender.init() == FR_OK ? HAL_OK : HAL_ERROR;
 }
 
 uint8_t Logger::log(float co2, float pressure, float humidity, float temperature1, float temperature2, float vdd) {
-	sprintf(logMessageBuffer, "%.2f,%.2f,%.1f,%.1f,%.1f,%.2f", (double) co2, (double) pressure, (double) humidity, (double) temperature1, (double) temperature2,
-			(double) vdd);
+	RTC_TimeTypeDef rtcTime;
+	RTC_DateTypeDef rtcDate;
+
+	HAL_RTC_GetTime(&hrtc, &rtcTime, RTC_FORMAT_BIN);
+	HAL_RTC_GetDate(&hrtc, &rtcDate, RTC_FORMAT_BIN);
+
+	sprintf(logMessageBuffer, "20%02d.%02d.%02d-%02d:%02d:%02d,%.2f,%.2f,%.1f,%.1f,%.1f,%.2f", rtcDate.Year, rtcDate.Month, rtcDate.Date, rtcTime.Hours,
+			rtcTime.Minutes, rtcTime.Seconds, (double) co2, (double) pressure, (double) humidity, (double) temperature1, (double) temperature2, (double) vdd);
 	return logLine(logMessageBuffer);
 }
 
